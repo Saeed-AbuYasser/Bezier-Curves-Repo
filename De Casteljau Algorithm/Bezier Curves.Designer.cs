917bb53e1ad1d354f3c1fd3cee2524c47fc3a245
Saeed <ratrakrable@gmail.com>
2025-11-21 07:35:31 +0300

    created the hole project

---
+++ De Casteljau Algorithm/Bezier Curves.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Graphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlDrawSquar = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnSetPoints = new System.Windows.Forms.Button();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlDrawSquar
+            // 
+            this.pnlDrawSquar.AutoScroll = true;
+            this.pnlDrawSquar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pnlDrawSquar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlDrawSquar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDrawSquar.Location = new System.Drawing.Point(0, 0);
+            this.pnlDrawSquar.Name = "pnlDrawSquar";
+            this.pnlDrawSquar.Size = new System.Drawing.Size(1069, 624);
+            this.pnlDrawSquar.TabIndex = 0;
+            this.pnlDrawSquar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlDrawSquar_MouseClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnDraw);
+            this.groupBox1.Controls.Add(this.btnSetPoints);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 624);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Script MT Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(62, 231);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(133, 63);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("Script MT Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDraw.Location = new System.Drawing.Point(44, 138);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(171, 63);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "De Casteljau";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnSetPoints
+            // 
+            this.btnSetPoints.Font = new System.Drawing.Font("Script MT Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetPoints.Location = new System.Drawing.Point(62, 45);
+            this.btnSetPoints.Name = "btnSetPoints";
+            this.btnSetPoints.Size = new System.Drawing.Size(133, 63);
+            this.btnSetPoints.TabIndex = 0;
+            this.btnSetPoints.Text = "Set Points";
+            this.btnSetPoints.UseVisualStyleBackColor = true;
+            this.btnSetPoints.Click += new System.EventHandler(this.btnSetPoints_Click);
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(426, 69);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(13, 13);
+            this.lblPoints.TabIndex = 0;
+            this.lblPoints.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 624);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pnlDrawSquar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bezier Curves";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDrawSquar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnSetPoints;
+        private System.Windows.Forms.Label lblPoints;
+    }
+}
+
---
+++ De Casteljau Algorithm/Bezier Curves.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+//De Casteljau's Algorithm to draw Bezier's Curves
+//saeed_293640 , ali_299393 , obaida_307066
+
+
+namespace Graphic
+{
+    public partial class Form1 : Form
+    {
+
+        //those are some essential variables to complete application's work
+        //---------------------
+        private byte Tracker = 0;
+        private PointF[] ControlPoints = new PointF[6];
+        private Label[] PointsLabelsContainer = new Label[6];
+        bool CanAccessDrawPanel = true;
+        //---------------------
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        // this function will write the point selected coordinates as a label control
+        //---------------------
+        private Label CreateLabel(String Text, MouseEventArgs e)
+        {
+            PointsLabelsContainer[Tracker] = new Label();
+            this.pnlDrawSquar.Controls.Add(PointsLabelsContainer[Tracker]);
+            PointsLabelsContainer[Tracker].Location = new Point(e.X + 6, e.Y - 8);
+            PointsLabelsContainer[Tracker].Text = Text;
+            PointsLabelsContainer[Tracker].BackColor = Color.Transparent;
+            PointsLabelsContainer[Tracker].AutoSize = true;
+            PointsLabelsContainer[Tracker].Font = new System.Drawing.Font("Script MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            PointsLabelsContainer[Tracker].ForeColor = System.Drawing.Color.Red;
+            PointsLabelsContainer[Tracker] = PointsLabelsContainer[Tracker];
+            return PointsLabelsContainer[Tracker];
+        }
+        //----------------------
+
+
+        // this procedure will remove all points, labels and curves.
+        //----------------------
+        private void ClearDrawPanel()
+        {
+            CanAccessDrawPanel = true;
+            ControlPoints = new PointF[6];
+            Graphics g = pnlDrawSquar.CreateGraphics();
+            g.Clear(pnlDrawSquar.BackColor);
+            for (int i = 0; i < Tracker; i++)
+            {
+                PointsLabelsContainer[i].Dispose();
+            }
+            PointsLabelsContainer = new Label[6];
+            Tracker = 0;
+            btnDraw.Enabled = true;
+        }
+        //----------------------
+
+
+
+        //this function is for finding the lower point's coordinates between the two upper points to form the Bezier Curve
+        //----------------------
+        private PointF NewCasteljauPoint(PointF point1, PointF point2,double t)
+        {
+            return new PointF(((point1.X) * (float)(1 - t) + point2.X * (float)t), ((point1.Y) * (float)(1 - t) + (point2.Y) * (float)t));
+        }
+        //----------------------
+
+
+        //and this one for drawing it
+        //----------------------
+        private void DrawCasteljauPoint(PointF point1,PointF point2,double t,Graphics g)
+        {
+            g.DrawEllipse(new Pen(Color.Red, 2), ((float)(point1.X) * (float)(1 - t) + (float)point2.X * (float)t), ((float)(point1.Y) * (float)(1 - t) + (float)point2.Y * (float)t), 2, 2);
+
+        }
+        //----------------------
+
+
+        //All the following procedures are for drawing different Degrees of Bezier Curves
+        //Note: I am the writer "Saeed Saleh" who code this algorithm all by myself just after understanding
+        //the De Casteljau's algorithm from some youtube videos. And to be honest, The explanation that is offered 
+        //on the university website was so frustrating, and this absolutely does not mean to disrespect any person
+        //but hopefully purposeful criticism.
+        //----------------------
+        private void DrawFirstDegreeCurve(PointF point1, PointF point2, Graphics g)
+        {
+            for (double t = 0; t <= 1; t = t + 0.001)
+            {
+                DrawCasteljauPoint(point1, point2, t, g);
+            }
+        }
+        private void DrawSecondDegreeCurve(PointF point1, PointF point2, PointF point3, Graphics g)
+        {
+            PointF point1_2 = new PointF();
+            PointF point2_3 = new PointF();
+
+            for (double t = 0; t <= 1; t += 0.001)
+            {
+                point1_2 = NewCasteljauPoint(point1,point2,t);
+                point2_3 = NewCasteljauPoint(point1, point3, t);
+                DrawCasteljauPoint(point1_2, point2_3, t, g);
+
+            }
+        }
+        private void DrawThirdDegreeCurve(PointF point1, PointF point2, PointF point3, PointF point4, Graphics g)
+        {
+            PointF point1_2, point2_3, point3_4;
+            PointF point1_2__2_3, point2_3__3_4;
+            for (double t = 0; t <= 1; t += 0.001)
+            {
+                point1_2 = NewCasteljauPoint(point1, point2, t);
+                point2_3 = NewCasteljauPoint(point2, point3, t);
+                point3_4 = NewCasteljauPoint(point3, point4, t);
+
+                point1_2__2_3 = NewCasteljauPoint(point1_2,point2_3,t);
+                point2_3__3_4 = NewCasteljauPoint(point2_3,point3_4,t);
+                DrawCasteljauPoint(point1_2__2_3,point2_3__3_4,t, g);
+            }
+        } 
+        private void DrawForthDegreeCurve(PointF point1, PointF point2, PointF point3, PointF point4,PointF point5, Graphics g)
+        {
+            PointF point1_2, point2_3, point3_4,point4_5;
+            PointF point1_2__2_3, point2_3__3_4,point3_4__4_5;
+            PointF point1_2__2_3__3_4, point2_3__3_4__4_5;
+            for (double t = 0; t <= 1; t += 0.001)
+            {
+                point1_2 = NewCasteljauPoint(point1,point2,t);
+                point2_3 = NewCasteljauPoint(point2,point3,t);
+                point3_4 = NewCasteljauPoint(point3,point4,t);
+                point4_5 = NewCasteljauPoint(point4,point5,t);
+
+
+                point1_2__2_3 = NewCasteljauPoint(point1_2, point2_3, t);
+                point2_3__3_4 = NewCasteljauPoint(point2_3, point3_4, t);
+                point3_4__4_5 = NewCasteljauPoint(point3_4, point4_5, t);
+
+
+                point1_2__2_3__3_4 = NewCasteljauPoint(point1_2__2_3, point2_3__3_4, t);
+                point2_3__3_4__4_5 = NewCasteljauPoint(point2_3__3_4, point3_4__4_5, t);
+
+                DrawCasteljauPoint(point1_2__2_3__3_4 , point2_3__3_4__4_5,t,g);
+            }
+        }
+        private void DrawFifthDegreeCurve(PointF point1, PointF point2, PointF point3, PointF point4, PointF point5,PointF point6, Graphics g)
+        {
+            PointF point1_2, point2_3, point3_4, point4_5,point5_6;
+            PointF point1_2__2_3, point2_3__3_4, point3_4__4_5, point4_5__5_6;
+            PointF point1_2__2_3__3_4, point2_3__3_4__4_5, point3_4__4_5__5_6;
+            PointF point1_2__2_3__3_4__4_5, point2_3__3_4__4_5__5_6;
+            for (double t = 0; t <= 1; t += 0.001)
+            {
+                point1_2 = NewCasteljauPoint(point1,point2,t);
+                point2_3 = NewCasteljauPoint(point2,point3,t);
+                point3_4 = NewCasteljauPoint(point3,point4,t);
+                point4_5 = NewCasteljauPoint(point4,point5,t);
+                point5_6 = NewCasteljauPoint(point5,point6,t);
+
+                point1_2__2_3 = NewCasteljauPoint(point1_2,point2_3,t);
+                point2_3__3_4 = NewCasteljauPoint(point2_3,point3_4,t);
+                point3_4__4_5 = NewCasteljauPoint(point3_4,point4_5,t);
+                point4_5__5_6 = NewCasteljauPoint(point4_5,point5_6,t);
+
+                point1_2__2_3__3_4 =NewCasteljauPoint( point1_2__2_3, point2_3__3_4, t);
+                point2_3__3_4__4_5 = NewCasteljauPoint(point2_3__3_4, point3_4__4_5, t);
+                point3_4__4_5__5_6 = NewCasteljauPoint(point3_4__4_5, point4_5__5_6, t);
+
+                point1_2__2_3__3_4__4_5 = NewCasteljauPoint(point1_2__2_3__3_4, point2_3__3_4__4_5, t);
+                point2_3__3_4__4_5__5_6 = NewCasteljauPoint(point2_3__3_4__4_5, point3_4__4_5__5_6, t);
+
+                DrawCasteljauPoint(point1_2__2_3__3_4__4_5, point2_3__3_4__4_5__5_6, t, g);
+            }
+        }
+        //----------------------
+
+ 
+        //and this procedure also I made it alone, which is for drawing a surrounding rectangle 
+        //----------------------
+        private void DrawRectangle(Pen pen ,PointF [] points,Graphics g)
+        {
+            float Biggest_X = points[0].X, Biggest_Y= points[0].Y, Smallest_X = points[0].X , Smallest_Y = points[0].Y;
+            for(int i = 1; i <Tracker; i++)
+            {
+                Biggest_X = Biggest_X > points[i].X ? Biggest_X : points[i].X;
+                Smallest_X = Smallest_X < points[i].X ? Smallest_X : points[i].X;
+                Biggest_Y = Biggest_Y > points[i].Y ? Biggest_Y : points[i].Y;
+                Smallest_Y = Smallest_Y < points[i].Y ? Smallest_Y : points[i].Y;
+            }
+            g.DrawRectangle(pen, Smallest_X, Smallest_Y,( Biggest_X - Smallest_X), (Biggest_Y - Smallest_Y));
+
+        }
+        //----------------------
+
+
+
+        //this procedure will call different Draw Procedure based on How many point the user choose to set.
+        private void DrawBezierCurves()
+        {
+            Graphics g = pnlDrawSquar.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 2);
+
+
+            //I know that it's easier to write the followings with Short Hand If, but it's faster with Switch Case Statement.
+            switch (Tracker)
+            {
+                case 2:
+                    DrawFirstDegreeCurve(ControlPoints[0], ControlPoints[1], g);
+                    DrawRectangle(pen, ControlPoints, g);
+                    break;
+                case 3:
+                    DrawFirstDegreeCurve(ControlPoints[0], ControlPoints[1], g);
+                    DrawSecondDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], g);
+                    DrawRectangle(pen, ControlPoints, g);
+                    break;
+                case 4:
+                    DrawFirstDegreeCurve(ControlPoints[0], ControlPoints[1], g);
+                    DrawSecondDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], g);
+                    DrawThirdDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], ControlPoints[3], g);
+                    DrawRectangle(pen, ControlPoints, g);
+                    break;
+                case 5:
+                    DrawFirstDegreeCurve(ControlPoints[0], ControlPoints[1], g);
+                    DrawSecondDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], g);
+                    DrawThirdDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], ControlPoints[3], g);
+                    DrawForthDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], ControlPoints[3],ControlPoints[4], g);
+                    DrawRectangle(pen, ControlPoints, g);
+                    break;
+                case 6:
+                    DrawFirstDegreeCurve(ControlPoints[0], ControlPoints[1], g);
+                    DrawSecondDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], g);
+                    DrawThirdDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], ControlPoints[3], g);
+                    DrawForthDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], ControlPoints[3], ControlPoints[4], g);
+                    DrawFifthDegreeCurve(ControlPoints[0], ControlPoints[1], ControlPoints[2], ControlPoints[3], ControlPoints[4],ControlPoints[5], g);
+                    DrawRectangle(pen, ControlPoints, g);
+                    break;
+                default:
+                    return;
+            }
+
+        }
+        //----------------------
+
+
+        //this proceduer will track the Mouse Click Event on the blue panel then will deal with different procedures
+        //to store, show the coordinates of and draw the point
+        //----------------------
+        private void pnlDrawSquar_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Tracker == 6 || !CanAccessDrawPanel)
+                return;
+
+            ControlPoints[Tracker] = new Point(e.X, e.Y);
+            CreateLabel($"({e.X},{e.Y})", e);
+            Graphics g = pnlDrawSquar.CreateGraphics();
+            g.DrawEllipse(new System.Drawing.Pen(Color.Red, 5), e.X, e.Y, 3, 3);
+            PointsLabelsContainer[Tracker].SendToBack();
+            Tracker++;
+            
+            pnlDrawSquar.SendToBack();
+
+        }
+        //----------------------
+
+
+        //Clicking on the button (Clear) well delete all the stored data and clean the screen then will back to defaults values
+        //----------------------
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearDrawPanel();
+
+        }
+        //----------------------
+
+
+        //this procedure will active the De Casteljau's Althorithm
+        //----------------------
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            if (Tracker <= 1)
+            {
+                MessageBox.Show("You must set at least two points!");
+                return;
+            }
+            CanAccessDrawPanel = false;
+            DrawBezierCurves();
+
+            btnDraw.Enabled = false;
+        }
+        //----------------------
+
+
+        //when clicking the Set Points button this going to allow you set points on the blue panel
+        //----------------------
+        private void btnSetPoints_Click(object sender, EventArgs e)
+        {
+            if (!CanAccessDrawPanel)
+            {
+                if (MessageBox.Show("To Set Points, You Should Clear The Panel First.\n Do You Want To Clear It?", "Error",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.OK)
+                    ClearDrawPanel();
+            }
+
+
+        }
+        //----------------------
+
+        
+    }
+}
